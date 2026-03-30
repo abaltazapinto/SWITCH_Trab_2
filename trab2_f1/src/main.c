b62d763382c775003c9d4caea7eb8992a897b2ee
abaltaza <andrebaltazarpinto@gmail.com>
2026-03-30 15:27:29 +0100

    ignore vscode settings

--- trab2_f1/src/main.c
+++ trab2_f1/src/main.c
@@ -4,29 +4,15 @@
 
 static void pwm_init(void)
 {
-    /* PB7 = OC2 como saída | PB7 = PWM (OC2) | PB5/ PB6 = direcao */
-    DDRB |= (1 << PB7) | (1 << PB5) | (1 << PB6);
+    /* PB7 = OC2 como saída */
+    DDRB |= (1 << PB7);
 
-    /* sentido fixo para teste: Dir0 = 1, Dir1 = 0 */
-    PORTB |= (1 << PB5);
-    PORTB &= ~(1 << PB6);
-
-    /*
-     * Timer2 em Fast PWM
-     * saída não invertida em OC2
-     * prescaler = 256
-     */
-    TCCR2 = (1 << WGM21) | (1 << WGM20)   /* Fast PWM */
-          | (1 << COM21)                  /* non-inverting */
-          | (1 << CS22)  | (1 << CS20);   /* prescaler 256 */
+    /* * Timer2 em Phase Correct PWM  |* saída não invertida |* prescaler = 64*/
+    TCCR2 = (1 << WGM20) | (1 << COM21) | (1 << CS21) | (1 << CS20)
 
     /* ~50% duty cycle */
     OCR2 = 128;
 
-    /* sentido fixo do motor: exemplo */
-    DDRB |= (1 << PB5) | (1 << PB6);
-    PORTB |= (1 << PB5);
-    PORTB &= ~(1 << PB6);
 }
 
 int main(void)
